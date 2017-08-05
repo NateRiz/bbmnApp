918bb1456ecbc2f6c6b801505e058cade1811016
NateRiz <riziknat@msu.edu>
2017-08-05 16:32:21 -0700

    Pictures offset fix

--- MealPrep/MealPrep/RecipeListViewController.swift
+++ MealPrep/MealPrep/RecipeListViewController.swift
@@ -12,9 +12,9 @@ class RecipeListViewController: UIViewController {
     
 
     @IBOutlet weak var recipeScrollView: UIScrollView!
+
     
     var imgCanvas = [String : UIImageView]()
-    
     var hits: NSArray?
     var query: String?
     
@@ -39,6 +39,67 @@ class RecipeListViewController: UIViewController {
     }
     
     
+    @IBAction func exitButton(_ sender: Any) {
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+    @IBAction func searchRecipes(_ sender: Any) {
+        
+        
+        
+    }
+    
+    func apiRequest()
+        /*
+         >Need to call api to get recipe data based on search data
+         >if query is nil defaults to chicken
+         */
+        
+    {
+        let query: String = self.query != nil ? self.query! : "Chicken"
+        
+        let urlBuilder = URL(string: "https://api.edamam.com/search?q="+query+"&app_id="+self.APPID+"&app_key="+self.APPKEY)
+        if let url = urlBuilder
+        {
+            let task = URLSession.shared.dataTask(with: url)
+            {
+                (data, response, error) in
+                if error != nil
+                {
+                    
+                    print(error!)
+                }
+                else
+                {
+                    if let usableData = data
+                    {
+                        print(usableData) //JsonSerialization
+                        
+                        do
+                        {
+                            let jsonResult = try JSONSerialization.jsonObject(with: usableData, options: JSONSerialization.ReadingOptions.mutableContainers) as AnyObject
+                            
+                            self.hits = jsonResult["hits"] as? NSArray
+                            print(self.hits!.count, "<<<")
+                            self.fillCollectionView()
+                            
+                        }
+                        catch
+                        {
+                            print("JSON processing failed...")
+                        }
+                        
+                        
+                    }
+                    
+                }
+            }
+            
+            task.resume()
+        }
+        
+        
+    }
     
     
     
@@ -60,6 +121,7 @@ class RecipeListViewController: UIViewController {
                     if let label = recipe["label"] as? String{
                         if let imageURL = recipe["image"] as? String{
                             //let recipeView = UIView(frame: CGRect(x: 0, y: 0, width: 300, height: 148))
+                            self.imgCanvas[label]=UIImageView()
                             getImage(imageURL, label)
                         }
                     }
@@ -72,15 +134,23 @@ class RecipeListViewController: UIViewController {
         {
             print(self.imgCanvas.count)
             
-            self.recipeScrollView.contentSize.height=CGFloat(64*(self.imgCanvas.count/2))
-            
             let pictureSize: Int = (Int(self.recipeScrollView.frame.width) - 4 - 4 - 4)/2
+            self.recipeScrollView.contentSize.height=CGFloat( 50 + pictureSize*(1+(self.imgCanvas.count/2)))
+            
             
             var increment: Int = 0
-            for (key,value) in self.imgCanvas
+            for (key, _) in self.imgCanvas
             {
-                self.imgCanvas[key]!.frame=CGRect(x: increment % 2 == 0 ? 4 : 8 + pictureSize, y: 5+(increment/2)*(pictureSize+10), width: pictureSize, height:pictureSize)
+                self.imgCanvas[key]!.frame=CGRect(x: increment % 2 == 0 ? 4 : 8 + pictureSize, y: 5 + (increment/2)*(pictureSize+10 + 50), width: pictureSize, height:pictureSize)
                 self.recipeScrollView.addSubview(self.imgCanvas[key]!)
+                
+                let recipeLabel = UILabel()
+                recipeLabel.text = key
+                recipeLabel.frame = CGRect(x: increment % 2 == 0 ? 4 : 8 + pictureSize, y: 5+(increment/2)*(pictureSize+10 + 50)+pictureSize, width: pictureSize, height: 50)
+                
+                self.recipeScrollView.addSubview(recipeLabel)
+                
+                
                 increment+=1
             }
         }
@@ -90,79 +160,29 @@ class RecipeListViewController: UIViewController {
     
     
     
-    func apiRequest()
-    /*
-         >Need to call api to get recipe data based on search data
-         >if query is nil defaults to chicken
-    */
-    
-    {
-        let query: String = self.query != nil ? self.query! : "Chicken"
-        
-        let urlBuilder = URL(string: "https://api.edamam.com/search?q="+query+"&app_id="+self.APPID+"&app_key="+self.APPKEY)
-        if let url = urlBuilder
-        {
-            let task = URLSession.shared.dataTask(with: url)
-            {
-                (data, response, error) in
-                if error != nil
-                {
-                
-                    print(error!)
-                }
-                else
-                {
-                    if let usableData = data
-                    {
-                        print(usableData) //JsonSerialization
-                        
-                        do
-                        {
-                            let jsonResult = try JSONSerialization.jsonObject(with: usableData, options: JSONSerialization.ReadingOptions.mutableContainers) as AnyObject
-
-                            self.hits = jsonResult["hits"] as? NSArray
-                            print(self.hits!.count, "<<<")
-                            self.fillCollectionView()
-        
-                        }
-                        catch
-                        {
-                            print("JSON processing failed...")
-                        }
-                    
-                    
-                    }
-                
-                }
-            }
-            
-            task.resume()
-        }
-    
-    
-    }
-    
     
     
     func getImage(_ imageURL: String, _ recipeLabel: String){
         let url = URL(string: imageURL)!
         let request = NSMutableURLRequest(url: url)
         
-        let _ = URLSession.shared.dataTask(with: request as URLRequest) { (data, response, error) in
+        let task = URLSession.shared.dataTask(with: request as URLRequest) { (data, response, error) in
             if error != nil {
                 print(error!)
             } else {
                 
                 if let data = data {
                     if let image = UIImage(data: data) {
-                        self.imgCanvas[recipeLabel] = UIImageView(image: image)
-                        print("got")
+                        DispatchQueue.main.async{
+                            self.imgCanvas[recipeLabel]!.image=image
+                            
+                        }
                     }
                 }
             }
         }
         
-        //task.resume()
+        task.resume()
     }
 
 }
