efabb366451d23240c9d6eed1f1314cbf2201028
NateRiz <matthewrizik@Matthews-iMac.lan>
2017-08-04 18:09:18 -0700

    recipe image stack view in new controller view WIP

--- MealPrep/MealPrep/RecipeListViewController.swift
+++ MealPrep/MealPrep/RecipeListViewController.swift
@@ -10,9 +10,15 @@ import UIKit
 
 class RecipeListViewController: UIViewController {
     
+
+    
+    @IBOutlet weak var recipeVerticalStack: UIStackView!
+    
+    var imgCanvas = [UIImageView]()
     
     var hits: NSArray?
     var query: String?
+    
     
     var APPKEY: String = "af7d9dd7a8371d144724086c9d95b91e"
     var APPID: String = "4474418e"
@@ -22,7 +28,10 @@ class RecipeListViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        
+        
         apiRequest()
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -32,7 +41,52 @@ class RecipeListViewController: UIViewController {
     
     
     
+    
+    
+    
+    
+    func fillCollectionView()
+    /*
+        >To fill scroll view with retrieved data from apiRequest()
+        >fills with UIImageViews, UIImages, Labels
+    */
+    {
+    
+        guard let hits=self.hits else {print("hits nil"); return}
+        if (hits.count == 0) {print("hits empty"); return}
+        
+        
+        for i in 0..<hits.count
+        {
+            if let hit = hits[i] as? [String : Any]{
+                if let recipe = hit["recipe"] as? [String : Any]{
+                    if let imageURL = recipe["image"] as? String{
+                        //let recipeView = UIView(frame: CGRect(x: 0, y: 0, width: 300, height: 148))
+                        self.imgCanvas.append(UIImageView(frame: CGRect(x: 0, y: 0, width: 300, height: 148)))
+                        getImage(imageURL, i)
+                    }
+                }
+            }
+            
+        }
+        DispatchQueue.main.async {
+            for i in 0..<self.imgCanvas.count{
+                self.recipeVerticalStack.addArrangedSubview(self.imgCanvas[i])
+            
+            }
+        }
+        
+    }
+    
+    
+    
+    
     func apiRequest()
+    /*
+         >Need to call api to get recipe data based on search data
+         >if query is nil defaults to chicken
+    */
+    
     {
         let query: String = self.query != nil ? self.query! : "Chicken"
         
@@ -59,6 +113,7 @@ class RecipeListViewController: UIViewController {
 
                             self.hits = jsonResult["hits"] as? NSArray
                             print(self.hits!.count, "<<<")
+                            self.fillCollectionView()
         
                         }
                         catch
@@ -78,7 +133,7 @@ class RecipeListViewController: UIViewController {
     
     }
     
-    func getImage(_ imageURL: String) {
+    func getImage(_ imageURL: String, _ canvasID: Int){
         let url = URL(string: imageURL)!
         let request = NSMutableURLRequest(url: url)
         
@@ -91,7 +146,7 @@ class RecipeListViewController: UIViewController {
                     if let image = UIImage(data: data) {
                         
                         DispatchQueue.main.async {
-                            //self.recipeImage.image = image
+                            self.imgCanvas[canvasID].image = image
                         }
                     }
                 }
