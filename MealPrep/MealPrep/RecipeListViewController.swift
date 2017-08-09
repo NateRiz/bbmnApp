02d6f26d667a771098b5b4a75b7a05c03daf030a
NateRiz <riziknat@msu.edu>
2017-08-09 16:20:57 -0400

    Stack3s with image/label in each

--- MealPrep/MealPrep/RecipeListViewController.swift
+++ MealPrep/MealPrep/RecipeListViewController.swift
@@ -119,7 +119,6 @@ class RecipeListViewController: UIViewController {
                 if let recipe = hit["recipe"] as? [String : Any]{
                     if let label = recipe["label"] as? String{
                         if let imageURL = recipe["image"] as? String{
-                            print("getting image")
                             getImage(imageURL, label)
                         }
                     }
@@ -128,29 +127,6 @@ class RecipeListViewController: UIViewController {
             }
             
         }
-        /*DispatchQueue.main.async
-        {
-            
-         
-         
-            
-            
-            var increment: Int = 0
-            for (key, _) in self.imgCanvas
-            {
-                self.imgCanvas[key]!.frame=CGRect(x: increment % 2 == 0 ? 4 : 8 + pictureSize, y: 5 + (increment/2)*(pictureSize+10 + 50), width: pictureSize, height:pictureSize)
-                self.recipeScrollView.addSubview(self.imgCanvas[key]!)
-                
-                let recipeLabel = UILabel()
-                recipeLabel.text = key
-                recipeLabel.frame = CGRect(x: increment % 2 == 0 ? 4 : 8 + pictureSize, y: 5+(increment/2)*(pictureSize+10 + 50)+pictureSize, width: pictureSize, height: 50)
-                
-                self.recipeScrollView.addSubview(recipeLabel)
-                
-                
-                increment+=1
-            }
-        }*/
         
     }
     
@@ -175,11 +151,10 @@ class RecipeListViewController: UIViewController {
                     if let image = UIImage(data: data) {
                         
                         newImageView.image=image
-                        print("appending")
-                        self.horizontalStackImageHolder["label"] = newImageView
+                        if self.horizontalStackImageHolder[recipeLabel] != nil{print("two recipes have the same name: Overwriting first...")}
+                        self.horizontalStackImageHolder[recipeLabel] = newImageView
                         if self.horizontalStackImageHolder.count == 2
                         {
-                            print("calling")
                             self.createNewStack(imgViews: self.horizontalStackImageHolder)
                             self.horizontalStackImageHolder.removeAll()
                         }
@@ -211,38 +186,35 @@ class RecipeListViewController: UIViewController {
             {
                 
                 let pictureSize: Int = (Int(self.recipeScrollView.frame.width) - 4 - 4 - 4) / 2
-                imgViews[labels[0]]!.frame = CGRect(x:0, y:0, width:pictureSize, height:pictureSize)
-                imgViews[labels[1]]!.frame = CGRect(x:0, y:0, width:pictureSize, height:pictureSize)
                 self.recipeScrollView.contentSize.height=CGFloat(pictureSize*(1+self.recipeStackView.subviews.count))
                 
                 
                 let newHorizStack=UIStackView()
                 newHorizStack.axis = UILayoutConstraintAxis(rawValue: 0)!
-                newHorizStack.frame = CGRect(x:CGFloat(0), y:CGFloat(self.recipeStackView.subviews.count * pictureSize), width:self.recipeStackView.frame.width, height: CGFloat(pictureSize))
+                newHorizStack.distribution = UIStackViewDistribution(rawValue: 1)!
                 newHorizStack.spacing = 4
                 
                 let leftVertStack = UIStackView()
                 leftVertStack.axis = UILayoutConstraintAxis(rawValue: 1)!
-                leftVertStack.frame = CGRect(x:0, y:0, width:pictureSize, height:pictureSize)
-                leftVertStack.addSubview(imgViews[labels[0]]!)
-                /*let leftLabel=UILabel()
+
+                leftVertStack.addArrangedSubview(imgViews[labels[0]]!)
+                let leftLabel=UILabel()
                 leftLabel.text=labels[0]
-                leftVertStack.addSubview(leftLabel)
-                newHorizStack.addSubview(leftVertStack)
-                 */
+                leftVertStack.addArrangedSubview(leftLabel)
+                newHorizStack.addArrangedSubview(leftVertStack)
+ 
                 let rightVertStack = UIStackView()
                 rightVertStack.axis = UILayoutConstraintAxis(rawValue: 1)!
-                rightVertStack.frame = CGRect(x:pictureSize, y:0, width:pictureSize, height:pictureSize)
-                rightVertStack.addSubview(imgViews[labels[1]]!)
-                /*let rightLabel=UILabel()
-                rightLabel.text=labels[0]
-                leftVertStack.addSubview(rightLabel)
-                newHorizStack.addSubview(rightVertStack)
-                */
+                rightVertStack.addArrangedSubview(imgViews[labels[1]]!)
+                let rightLabel=UILabel()
+                rightLabel.text=labels[1]
+                rightVertStack.addArrangedSubview(rightLabel)
+                newHorizStack.addArrangedSubview(rightVertStack)
+ 
                 
                 
-                self.recipeStackView.addSubview(newHorizStack)
-                print("added")
+                self.recipeStackView.addArrangedSubview(newHorizStack)
+                print("new",self.recipeScrollView.contentSize.height)
                 
                 
         }
